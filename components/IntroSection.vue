9699df8841b67882ed42e37c38580259f74fd2c8
Thomas Kohlmannhuber <thomas.kohlmannhuber@yahoo.de>
2021-10-28 19:36:07 +0200

    deploy

--- components/IntroSection.vue
+++ components/IntroSection.vue
@@ -1,18 +1,26 @@
 <template>
   <section class="wrapper">
-    <Circles />
+    <Circles class="circle-top" />
     <IntroText />
-    <SingleCircle class="circle" />
+    <SingleCircle class="circle-bottom" />
   </section>
 </template>
 
 <style lang="scss" scoped>
+@import '~assets/scss/main';
+
 section {
   display: flex;
   flex-direction: column;
   gap: var(--gap);
+  margin-bottom: var(--section-spacing);
 
-  .circle {
+  .circle-top {
+    @include media('>=md') {
+      align-self: center;
+    }
+  }
+  .circle-bottom {
     align-self: center;
   }
 }
